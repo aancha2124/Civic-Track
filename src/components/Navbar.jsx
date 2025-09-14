3a008d6cb9f297d6ac838ddb2535760de96134a9
aanchal21 <aanchalbhargava2005@gmail.com>
2025-09-14 23:59:51 +0530

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,16 @@ function Navbar({ setCurrentPage }) {
   return (
     <nav>
       <h1>CivicTrack</h1>
-      <ul>
-        <li><a href="#" onClick={() => setCurrentPage('home')}>Home</a></li>
-        <li><a href="#" onClick={() => setCurrentPage('report')}>Report Issue</a></li>
-        <li><a href="#" onClick={() => setCurrentPage('track')}>Track Issues</a></li>
-        <li><a href="#" onClick={() => setCurrentPage('community')}>Community</a></li>
-        <li><a href="#" onClick={() => setCurrentPage('about')}>About</a></li>
-      </ul>
-      <button onClick={() => setCurrentPage('login')}>Login / Signup</button>
+      <div className="nav-right">
+        <ul>
+          <li><a href="#" onClick={() => setCurrentPage('home')}>Home</a></li>
+          <li><a href="#" onClick={() => setCurrentPage('report')}>Report Issue</a></li>
+          <li><a href="#" onClick={() => setCurrentPage('track')}>Track Issues</a></li>
+          <li><a href="#" onClick={() => setCurrentPage('community')}>Community</a></li>
+          <li><a href="#" onClick={() => setCurrentPage('about')}>About</a></li>
+        </ul>
+        <button onClick={() => setCurrentPage('login')}>Login / Signup</button>
+      </div>
     </nav>
   );
 }
